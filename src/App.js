597cc32ba56019452771108e62530760ffa9614c
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-25 17:07:02 +0200

    2nd Context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Fragment } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
-import CategoriasProvider from "./context/CategoriaContext"
+import CategoriasProvider from "./context/CategoriaContext";
+import RecetasProvider from "./context/RecetasContext";
 
 function App() {
   return (
     <CategoriasProvider>
-      <Header />
-      <div className="container mt-5">
-        <div className="row">
-          <Formulario />
+      <RecetasProvider>
+        <Header />
+        <div className="container mt-5">
+          <div className="row">
+            <Formulario />
+          </div>
         </div>
-      </div>
+      </RecetasProvider>
     </CategoriasProvider>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import { CategoriasContext } from "../context/CategoriaContext";
+import { RecetasContext } from "../context/RecetasContext";
 
 const Formulario = () => {
   // State en sólo este componente.
@@ -9,6 +10,7 @@ const Formulario = () => {
   });
 
   const { categorias } = useContext(CategoriasContext);
+  const { buscarRecetas } = useContext(RecetasContext);
 
   // Función para leer los contenidos.
   const obtenerDatosReceta = (e) => {
@@ -19,7 +21,13 @@ const Formulario = () => {
   };
 
   return (
-    <form className="col-12">
+    <form
+      className="col-12"
+      onSubmit={(e) => {
+        e.preventDefault();
+        buscarRecetas(busqueda);
+      }}
+    >
       <fieldset className="text-center">
         <legend>Find drinks by category or ingredient</legend>
       </fieldset>
